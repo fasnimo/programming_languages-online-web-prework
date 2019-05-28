d46a0061c270b87685ad2ee5515e2dad023a1d3d
Rene Cocom <fasnimo@gmail.com>
2019-05-28 22:47:03 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,6 @@
 def reformat_languages(languages)
   new_hash = {}
-  languages[:style] = [:oo].each do |key, value|
+  languages.each do |key, value|
     puts key 
     value.each do |k, v|
       puts k 
