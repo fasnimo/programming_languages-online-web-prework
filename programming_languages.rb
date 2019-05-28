bf0c0d0b31520d8f42e5595e4bd81904d56ff2b9
Rene Cocom <fasnimo@gmail.com>
2019-05-28 23:05:06 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,9 +3,9 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |oo_functional, languages_hash|
    
-    languages_hash.each do |k, v|
+    languages_hash.each do |language, hash|
        binding.pry
-      puts k 
+      puts k
       puts v 
     end 
   end
